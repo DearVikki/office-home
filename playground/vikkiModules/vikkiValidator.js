f1bbfbf865e763322a4b82ded269a4a20a32469f
DearVikki <wanqicn@qq.com>
2016-09-06 17:20:02 +0800

    (｡･ω･｡)

--- playground/vikkiModules/vikkiValidator.js
+++ playground/vikkiModules/vikkiValidator.js
@@ -1,6 +1,7 @@
 function VikkiValidator(container, rules) {
     this.errMsg = [];
     this.rulesObj = {};
+    this.rulesDefault = ['isRequired', 'rangeCheck', 'lengthCheck'];
     //空值判断
     this.isRequired = function(el) {
         if (el.value.length > 0) {
@@ -12,24 +13,33 @@ function VikkiValidator(container, rules) {
         }
     };
     //长度判断
-    this.lengthCheck = function(el, len, msg){
-    	var max = len.max||Infinity;
-    	var min = len.min||0;
-    	//if(el.value.length >= len)
+    this.min = function(el, len, msg){
+    	if(el.value.length < len){
+    		var evt = new CustomEvent('validatePass',{'bubbles':true});
+            el.dispatchEvent(evt);
+    	} else {
+    		var evt = new CustomEvent('validateFail',{'bubbles':true, 'detail':'too short'});
+            el.dispatchEvent(evt);
+    	}
     };
     //范围判断
     this.rangeCheck = function(){};
 
     this.check = function(e){
+    	console.log(this)
     	var e = e||window.event;
     	var target = e.target;
     	var targetName = e.target.name;
     	var rule = this.rulesObj[targetName];
     	var ruleName = Object.keys(rule);
-    	ruleName.forEach(function(name){
+    	/*ruleName.forEach(function(name){
     		if(this[name]) this[name](target, rule[ruleName]);
-    	}.bind(this))
-    }.bind(this)
+    	}.bind(this))*/
+    	this.rulesDefault.forEach(function(i){
+    		console.log(this)
+    		if(i in rule) this[i](target, rule[i]);
+    	}).bind(this);
+    }.bind(this);
 
     for(var i=0; i<rules.length; i++){
     	var elName = rules[i].field.name;
@@ -39,6 +49,7 @@ function VikkiValidator(container, rules) {
 
     var inputs = container.querySelectorAll('input');
     for(var i=0; i<inputs.length; i++){
+    	console.log(inputs[i])
     	inputs[i].onblur = this.check;
     }
 }
