467160ecee1c61ddca0a4f451dba71d763b34dc7
Vikki Dear <Vikki Dear>
2016-10-08 09:49:49 +0800

    (ಥ_ಥ) 又要上班了…我还想做一只宝宝…

--- playground/draft/webpackTrying3/src/app.js
+++ playground/draft/webpackTrying3/src/app.js
@@ -1,7 +1,10 @@
 require('./css/style.css');
+require('./css/test.less');
 var yell = require('./js/alert.js');
 //为什么会在渲染颜色之前运行?
-yell('world')
+//yell('world')
+alert('123');
 require(['./js/alert.js'],function(good){
-	good('worlddd');
+	//good('worlddd');
+	console.log('12312asdsadsad');
 })
--- playground/draft/webpackTrying3/webpack.config.js
+++ playground/draft/webpackTrying3/webpack.config.js
@@ -1,12 +1,16 @@
 module.exports = {
-  entry: './src/app.js',
-  output: {
-    path: './dist',
-    filename: 'bundle.js'
-  },
-  module: {
-    loaders: [
-      { test: /\.css$/, loader: 'style!css' }
-    ]
-  }
+    entry: ['./src/app.js', 'webpack-dev-server/client?http://localhost:8080'],
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [{
+            test: /\.css$/,
+            loader: 'style!css'
+        }, {
+            test: /\.less$/,
+            loader: "style!css!less"
+        }]
+    }
 };
