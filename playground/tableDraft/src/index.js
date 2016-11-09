2c370571203f188c7d377985e4bc1c3a3830df12
DearVikki <wanqicn@qq.com>
2016-11-09 19:21:33 +0800

    回去继续写table喔~

--- playground/tableDraft/src/index.js
+++ playground/tableDraft/src/index.js
@@ -1,13 +1,40 @@
 var Vue = require('vue');
 require('./style/index.css');
+var Mock = require('mockjs');
+var data = Mock.mock({
+	'items|40':[{
+		'animal|1':['doggy','caaaaaat'],
+		'legs|1':['four','我也有四条腿喔嘻嘻嘻嘻'],
+		'food|1':['bones','fish'],
+		'cute':true
+	}]
+})
 var v = new Vue({
 	el:'#app',
 	data:{
 		msg:'Hello you',
-		items:[{animal:'dogggy', legs: 'four', food:'bones', cute:true},
-		{animal:'caaaaaaaaaaat', legs:'fourrrrrrrrrrrrrrrrrrrrrrrrrrrr', food:'fish', cute:true}
-
-		]
+		items:data.items,
+		top:'',
+		left:'',
+		fix:false,
+		width1:'',
+		width2:'',
+		width3:'',
+		width4:''
+	},
+	mounted(){
+		this.width1 = this.$refs.animal.getBoundingClientRect().width+'px';
+		this.width2 = this.$refs.legs.getBoundingClientRect().width+'px';
+		this.width3 = this.$refs.food.getBoundingClientRect().width+'px';
+		this.width4 = this.$refs.cute.getBoundingClientRect().width+'px';
+		this.left = this.$refs.thead.getBoundingClientRect().left;
+		this.top = this.$refs.thead.getBoundingClientRect().top;
+		window.addEventListener('scroll',this.handleScroll);
+	},
+	methods:{
+		handleScroll(){
+			if(window.scrollY > this.top) this.fix = true;
+			else this.fix=false;
+		}
 	}
 })
-console.log(v)
