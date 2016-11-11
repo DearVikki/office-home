920337711724aab4b5679999bd18e829a3369266
DearVikki <wanqicn@qq.com>
2016-11-11 18:08:07 +0800

    阿今天我要病死了…

--- playground/chompre/my-project/src/module/vali/Vali.vue
+++ playground/chompre/my-project/src/module/vali/Vali.vue
@@ -1,15 +1,43 @@
 <template>
 	<div>
-		<p>{{msg}}</p>
+		<validate name="validation1">
+	      <div class="username-field">
+	        <label for="username">username:</label>
+	        <validity field="username" :validators="['required']">
+	        	<input id="username" type="text">
+	        </validity>
+	      </div>
+	      <div class="comment-field">
+	        <label for="comment">comment:</label>
+	        <validity field="comment" v-validate:comment="{maxlength: 7}">
+	        	<input id="comment" type="text">
+	        </validity>
+	      </div>
+	      <div class="errors">
+	        <p v-if="usernameRequire">Required your name.</p>
+	        <p v-if="commentLong">Your comment is too long.</p>
+	      </div>
+	      <input type="submit" value="send" v-if="allPass">
+	  </validate>
 	</div>
 </template>
 <script>
+	import validate from 'vue-validator';
 	export default{
 		name:'Vali',
 		data(){
 			return{
 				msg:'If you see this, then congrats!!'
 			}
-		}
+		},
+		computed: validate.mapValidation({
+			usernameRequire: '$validation.validation1.username.required',
+			commentLong: '$validation.validation1.comment.maxlength',
+			allPass:'$validation.validation1.valid'
+		}),
+		mounted(){
+			//console.log(validate)
+		},
+		components:{validate}
 	}
 </script>
--- playground/chompre/my-project/src/module/vali/vali.js
+++ playground/chompre/my-project/src/module/vali/vali.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Vali from './Vali.vue'
+/*import VueValidator from 'vue-validator'
+
+Vue.use(VueValidator);*/
+
 new Vue({
   el: '#app',
   components:{Vali}
