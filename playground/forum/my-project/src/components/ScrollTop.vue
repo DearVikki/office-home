dddf94f0a6c4fdef4c135c3bd64d5c565860c377
Vikki Dear <Vikki Dear>
2017-04-23 14:27:29 +0800

    ...的习惯- -

---
+++ playground/forum/my-project/src/components/ScrollTop.vue
@@ -0,0 +1,44 @@
+<template>
+	<transition name="fade">
+		<div id="scroll_top" @click="scrollTop" v-show="show">♥</div>
+	</transition>
+</template>
+<script>
+	export default{
+		name:'scrollTop',
+		data(){
+			return {
+				show:false,
+				top:''
+			}
+		},
+		mounted(){
+			window.addEventListener('scroll', this.checkScrollTop, false);
+		},
+		methods:{
+			checkScrollTop(){
+				this.show = document.body.scrollTop > 700 ? true : false;
+			},
+			scrollTop(){
+				this.top = document.body.scrollTop;
+				this.animaFrame();
+			},
+			animaFrame(){
+				this.top -= 50;
+				window.scrollTo(0, top);
+				if(top > 0){
+				    window.requestAnimationFrame(this.animaFrame);
+				}
+			}
+		}
+	}
+</script>
+<style lang='less' scoped>
+	#scroll_top{
+		position: fixed;
+		right: .5rem;
+		bottom: 2rem;
+		font-size: 1.5rem;
+		color: #F9C13A;
+	}
+</style>
