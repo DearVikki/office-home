60ee0b6646ae6c7358e087462ca7a2e5b8de517c
vikki <wanqicn@qq.com>
2017-04-28 09:44:57 +0800

    还没想出来…

--- playground/test/my-project/src/module/leancloud/Le.vue
+++ playground/test/my-project/src/module/leancloud/Le.vue
@@ -1,28 +1,64 @@
 <template>
 	<div>
 		<input type="file" @change="fileUpload">
+		<textarea v-model="textarea"></textarea>
+		<button @click="submitTextarea">提交textarea</button>
+		<div id="contentedit" style="background: pink;white-space: pre-wrap" contenteditable=true></div>
+		<p>{{textarea}}</p>
+		<p id="test" style="white-space: pre"></p>
+		<button @click="submitDiv">提交contenteditable</button>
 	</div>
 </template>
 <script>
 	import AV from 'leancloud-storage'
 	export default{
 		name:'le',
+		data(){
+			return {
+				textarea:'',
+				To:''
+			}
+		},
 		mounted(){
 			const appId = '1zD4Kio5pFbNgnTpaRJ8nTnz-gzGzoHsz';
 			const appKey = '3m3Du5FwDHR8mMvleV0CxAML';
 			AV.init({appId, appKey});
 
-			const TO = AV.Object.extend('TestObject');
-			const to = new TO();
-			// ah! await居然暂时还用不了 说是reserved word
-			// await to.save({words:'hhh'});
-			// to.save({
-			// 	words:'hhh2'
-			// }).then(()=>{
-			// 	console.log('Leancloud Rocks!')
-			// })
+			this.To = AV.Object.extend('TestObject');
+
+			var query = new AV.Query(this.To);
+			query.get('58d556fa44d9040068688549').then((response)=>{
+				this.textarea = response.get('words');
+				var text =  this.textarea.replace(/\r?\n/g, '<br />');
+				document.querySelector('#test').innerHTML = this.textarea;
+				document.querySelector('#contentedit').innerHTML = this.textarea;
+			})
 		},
 		methods:{
+			submitTextarea(){
+				console.log(this.textarea)
+				const to = new this.To();
+				to.set('words',this.textarea);
+				to.save().then((response)=>{
+					console.log(response)
+				})
+			},
+			submitDiv(){
+				// const to = new this.To();
+				// to.set('words',this.textarea);
+				// to.save().then((response)=>{
+				// 	console.log(response)
+				// })
+				var text = document.querySelector('#contentedit')
+				console.log(text.innerHTML)
+				console.log(text.textContent)
+				console.log(text.innerText)
+				// const to = new this.To();
+				// to.set('words',text.innerText);
+				// to.save().then((response)=>{
+				// 	console.log(response)
+				// })
+			},
 			fileUpload(e){
 				var files= e.target.files[0];
 				// if(!files.length) return;
