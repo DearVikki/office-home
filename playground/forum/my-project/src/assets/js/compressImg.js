4cbc3352814720a30bce8c9085a5b773f3182bc7
Vikki Dear <Vikki Dear>
2017-06-27 23:51:53 +0800

    xx

--- playground/forum/my-project/src/assets/js/compressImg.js
+++ playground/forum/my-project/src/assets/js/compressImg.js
@@ -34,14 +34,7 @@ export default function(img, type){
             ubuffer[i] = text.charCodeAt(i);
         }
 
-
         var blob = new Blob([buffer], {type: type});
-<<<<<<< HEAD
         blob.lastModifiedDate = new Date();
         return blob;
-=======
-        // console.log(blob)
-        var file = new File([blob], "name.JPG", {type:"image/jpeg"})
-        return file;
->>>>>>> 448d932d1461c01d79522431440b16d6ad8e7897
 }
